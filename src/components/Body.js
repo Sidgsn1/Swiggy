c68bf04b97b459996097ff79169765ef2f095842
Sidgsn1 <sidgsn101@gmail.com>
2026-03-14 22:06:44 +0530

    shimmerUiAdded

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 import RestaurantCard from './RestaurantCard';
-import resData from '../utils/mockData';
+import Shimmer from './Shimmer';
+// import resData from '../utils/mockData';
 
 const Body = () => {
   //creating a useState hook
-  const [resList,setResList]=useState(resData);
+  const [resList,setResList]=useState([]);
+  const [allResList,setAllResList]=useState([]); //used to store all resList original when clicking showall
   const [isFiltered,setIsFiltered]=useState(false);
 
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+
+  const fetchData=async()=>{
+    const data=await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    )
+    const json=await data.json();
+    console.log(json);
+    
+    //optional chaining
+    const restaurantCards=json?.data?.cards?.find((card)=>card?.card?.card?.gridElements?.infoWithStyle?.restaurants);//abhi mujhe vo card[i] mila ha jisme actually restaurants hai
+    //ab mujhe usme se vo restaurants nikalna hoga
+    const restaurants=restaurantCards?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+    //now that i got the restaurants obj now i will setResList
+    console.log(restaurantCards.card.card.gridElements.infoWithStyle.restaurants.length);
+    setResList(restaurants);
+    setAllResList(restaurants);
+  }
   const handleFilter=()=>{
     if(isFiltered===true){
-      setResList(resData);
+      setResList(allResList);
       setIsFiltered(false);
     }
     else{
@@ -20,7 +44,7 @@ const Body = () => {
       setIsFiltered(true);
     }
   }
-  return (
+  return resList.length==0?<Shimmer /> : (
     <div className="res-container">
       <div className="res-container-head">
         <h1>Restaurants with online food delivery in Delhi</h1>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,76 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='shimmer-container'>
+        <div className='shimmer-head'>
+            <div className='loading-icon'>
+                <div className='loader'></div>
+                <i className="ri-bowl-line"></i>
+            </div>
+            <h1>Looking for great food near you ...</h1>
+        </div>
+        <div className='shimmer-body'>
+            <div className='shimmer-left'>
+                <div className='left-elem'>
+                    <div className='left-circle'></div>
+                    <div className='left-rec'></div>
+                </div>
+                <div className='left-elem'>
+                    <div className='left-circle'></div>
+                    <div className='left-rec'></div>
+                </div>
+                <div className='left-elem'>
+                    <div className='left-circle'></div>
+                    <div className='left-rec'></div>
+                </div>
+                <div className='left-elem'>
+                    <div className='left-circle'></div>
+                    <div className='left-rec'></div>
+                </div>
+                <div className='left-elem'>
+                    <div className='left-circle'></div>
+                    <div className='left-rec'></div>
+                </div>
+            </div>
+            <div className='shimmer-right'>
+                <div className='right-filter'></div>
+                <div className='right-card-container'>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                    <div className='right-card'>
+                        <div className='r-card-img'></div>
+                        <div className='r-card-h1'></div>
+                        <div className='r-card-h2'></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Shimmer
